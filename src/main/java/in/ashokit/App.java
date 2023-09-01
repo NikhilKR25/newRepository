198b2a13864afa0fc2e27b9b84dab3c7621ab60c
Nikhil Kuthe <nikhil.rkuthe@gmail.com>
2023-09-01 19:50:52 +0530

    app updated

--- src/main/java/in/ashokit/App.java
+++ src/main/java/in/ashokit/App.java
@@ -9,5 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "New File is Updated" );
+        int value = 20;
+        System.out.println( value);
     }
 }
